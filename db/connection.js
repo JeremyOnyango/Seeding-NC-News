5e99c1b4eea40587954f44cd4146f7516d476589
root <root@JsAsus.>
2025-03-14 16:03:03 +0000

    hosting

--- db/connection.js
+++ db/connection.js
@@ -7,11 +7,18 @@ require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
 const db = new Pool();
 
-if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
+if (!process.env.PGDATABASE && !process.env.PGDATABASE_URL) {
+    throw new Error("PGDATABASE or DATABASE_URL not set")
 } else { 
     console.log(`Connected to ${process.env.PGDATABASE}`)
+}
+const config = {};
+
+if (ENV === "production"){
+    config.connectionString = process.env.PGDATABASE_URL;
+    config.max = 2;
 }
 
 
 module.exports = db;
+module.exports = new Pool(config);
